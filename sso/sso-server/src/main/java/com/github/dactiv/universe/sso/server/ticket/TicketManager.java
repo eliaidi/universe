3ac3eda7f993d79ae8f529e244cb9edb356aff94
maurice <maurice.chen@foxmail.com>
2016-03-09 21:23:37 +0800

    重构一下票据验证管理器.

--- sso/sso-server/src/main/java/com/github/dactiv/universe/sso/server/ticket/TicketManager.java
+++ sso/sso-server/src/main/java/com/github/dactiv/universe/sso/server/ticket/TicketManager.java
@@ -26,7 +26,7 @@ import org.apache.shiro.authc.AuthenticationToken;
  *
  * @author maurice
  */
-public interface TicketManager{
+public interface TicketManager extends TicketValidationManager{
 
     /**
      * 生成一个新的票据
